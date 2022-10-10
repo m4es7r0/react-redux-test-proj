8d3473452bcb74fa59c9afe62837001c782035a8
m4e7r0 <dodogof02@gmail.com>
2022-10-10 18:08:40 +0300

    min fix action fetch

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 export const fetchHeroes = (request) => (dispatch) => {
+    dispatch(heroesFetching())
     request("http://localhost:3001/heroes")
         .then(data => dispatch(heroesFetched(data)))
         .catch(() => dispatch(heroesFetchingError()))
