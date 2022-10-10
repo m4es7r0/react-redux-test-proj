9f87a96d185976cdb5e315088f0e68cdd5b30bad
m4e7r0 <dodogof02@gmail.com>
2022-10-10 16:42:56 +0300

    applyMiddleware, compose

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,30 @@
-import { createStore, combineReducers, compose } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import { filters, heroes } from '../reducers/index';
 
-const enhancer = (createStore) => (...args) => {
-    const store = createStore(...args);
-    const oldDispatch = store.dispatch;
 
-    store.dispatch = (action) => {
-        if (typeof action === 'string') return oldDispatch({ type: action })
-        else return oldDispatch(action)
-    }
-
-    return store
+//// MiddleWare взаемодействует только с Dispatch
+const stringMiddleWare = (store) => (dispatch) => (action) => {
+    if (typeof action === 'string') return dispatch({ type: action })
+    else return dispatch(action)
 }
+
+//// Enhancer изменяет store и его работу 
+// const enhancer = (createStore) => (...args) => {
+//     const store = createStore(...args);
+//     const oldDispatch = store.dispatch;
+
+//     store.dispatch = (action) => {
+//         if (typeof action === 'string') return oldDispatch({ type: action })
+//         else return oldDispatch(action)
+//     }
+
+//     return store
+// }
 
 const store = createStore(
     combineReducers({ heroes, filters }),
     compose(
-        enhancer,
+        applyMiddleware(stringMiddleWare),
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     ));
 
