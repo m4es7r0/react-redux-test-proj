03bb9fd184c608c7522fe08aa6e89c46f7fb7417
m4e7r0 <dodogof02@gmail.com>
2022-10-12 16:49:02 +0300

    createAsyncThunk

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -1,11 +1,10 @@
-import {useHttp} from '../../hooks/http.hook';
+import { useHttp } from '../../hooks/http.hook';
 import { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { CSSTransition, TransitionGroup} from 'react-transition-group';
-import { createSelector } from 'reselect';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { createSelector } from '@reduxjs/toolkit';
 
-import { fetchHeroes } from '../../actions';
-import { heroDeleted } from './heroesSlice';
+import { fetchHeroes, heroDeleted } from './heroesSlice';
 
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
@@ -29,23 +28,22 @@ const HeroesList = () => {
     const filteredHeroes = useSelector(filteredHeroesSelector);
     const heroesLoadingStatus = useSelector(state => state.heroes.heroesLoadingStatus);
     const dispatch = useDispatch();
-    const {request} = useHttp();
+    const { request } = useHttp();
 
     useEffect(() => {
-        dispatch(fetchHeroes(request));
+        dispatch(fetchHeroes());
         // eslint-disable-next-line
     }, []);
 
     const onDelete = useCallback((id) => {
         request(`http://localhost:3001/heroes/${id}`, "DELETE")
-            .then(data => console.log(data, 'Deleted'))
             .then(dispatch(heroDeleted(id)))
             .catch(err => console.log(err));
         // eslint-disable-next-line  
     }, [request]);
 
     if (heroesLoadingStatus === "loading") {
-        return <Spinner/>;
+        return <Spinner />;
     } else if (heroesLoadingStatus === "error") {
         return <h5 className="text-center mt-5">Ошибка загрузки</h5>
     }
@@ -61,13 +59,13 @@ const HeroesList = () => {
             )
         }
 
-        return arr.map(({id, ...props}) => {
+        return arr.map(({ id, ...props }) => {
             return (
-                <CSSTransition 
+                <CSSTransition
                     key={id}
                     timeout={500}
                     classNames="hero">
-                    <HeroesListItem  {...props} onDelete={() => onDelete(id)}/>
+                    <HeroesListItem  {...props} onDelete={() => onDelete(id)} />
                 </CSSTransition>
             )
         })
--- src/components/heroesList/heroesSlice.js
+++ src/components/heroesList/heroesSlice.js
@@ -1,4 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { useHttp } from '../../hooks/http.hook';
+
+export const fetchHeroes = createAsyncThunk(
+    'heroes/fetchHeroes',
+    () => {
+        const { request } = useHttp()
+        return request("http://localhost:3001/heroes")
+    }
+)
 
 const heroesSlice = createSlice({
     name: 'heroes',
@@ -7,20 +16,22 @@ const heroesSlice = createSlice({
         heroesLoadingStatus: 'idle'
     },
     reducers: {
-        heroesFetching: state => { state.heroesLoadingStatus = 'loading' },
-        heroesFetched: (state, action) => {
-            state.heroesLoadingStatus = 'idle';
-            state.heroes = action.payload;
-        },
-        heroesFetchingError: state => {
-            state.heroesLoadingStatus = 'error';
-        },
         heroCreated: (state, action) => {
             state.heroes.push(action.payload);
         },
         heroDeleted: (state, action) => {
             state.heroes = state.heroes.filter(item => item.id !== action.payload);
         }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchHeroes.pending, state => { state.heroesLoadingStatus = 'loading' })
+            .addCase(fetchHeroes.rejected, state => { state.heroesLoadingStatus = 'error' })
+            .addCase(fetchHeroes.fulfilled, (state, action) => {
+                state.heroesLoadingStatus = 'idle';
+                state.heroes = action.payload;
+            })
+            .addDefaultCase(() => { })
     }
 });
 
